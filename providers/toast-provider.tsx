e3e5db9228204d9f9b259a70af7eca073394f53d
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2024-04-20 03:28:04 -0230

    fix the bug of building app

--- providers/toast-provider.tsx
+++ providers/toast-provider.tsx
@@ -1,18 +1,21 @@
-"use client"
-
-import { ToastContainer } from "react-toastify"
+import { ToastContainer } from "react-toastify";
 import { useMediaQuery } from "@uidotdev/usehooks";
-import "react-toastify/dist/ReactToastify.css"
+import "react-toastify/dist/ReactToastify.css";
 
 const ToastProvider = () => {
-  const isSmallDevice  = useMediaQuery("only screen and (max-width : 768px)");
-  return (
-    <ToastContainer
-      toastClassName="toast-message"
-      position={isSmallDevice  ? "bottom-left" : "top-right"}
-      draggable
-    />
-  )
-}
+  // Check if window is defined before using client-side hooks
+  if (typeof window !== 'undefined') {
+    const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
+    return (
+      <ToastContainer
+        toastClassName="toast-message"
+        position={isSmallDevice ? "bottom-left" : "top-right"}
+        draggable
+      />
+    );
+  }
+  // Return null if running on the server-side
+  return null;
+};
 
-export default ToastProvider
+export default ToastProvider;
