33ab06808594a9ffeee7c360e6b0be89749181d3
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2024-04-27 07:05:32 -0230

    fix the bug of build

--- app/[lang]/intro/page.tsx
+++ app/[lang]/intro/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { Logo } from "@/components/layouts/Logo"
+import { Logo } from "@/components/layouts/logo"
 import { useTranslations } from "next-intl"
 
 export default function Intro() {
--- components/layouts/footer/Footer.tsx
+++ components/layouts/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Logo } from "../Logo"
+import { Logo } from "../logo"
 import { HiChevronRight } from "react-icons/hi2"
 import Link from "next/link"
 import NewsLetter from "./news-letter"
